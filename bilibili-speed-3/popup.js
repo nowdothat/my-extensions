b36c82b5e6304f82e3f7eb83bbeea54bc898e689
Ken <675062812@QQ.com>
2024-10-13 22:57:33 +0800

    chore: fast update

--- bilibili-speed-3/popup.js
+++ bilibili-speed-3/popup.js
@@ -1,4 +1,22 @@
 // popup.js
+// 获取按钮容器元素
+const buttonContainer = document.getElementById('button-container');
+
+// 循环生成 1 到 10 的按钮
+for (let i = 1; i <= 10; i++) {
+    // 创建一个新的按钮元素
+    const button = document.createElement('button');
+
+    // 设置按钮的类名和值
+    button.className = 'c-button';
+    button.value = i.toString(); // 将数字转换为字符串
+    button.textContent = i; // 设置按钮的文本内容
+
+    // 将按钮添加到容器中
+    buttonContainer.appendChild(button);
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var buttonElements = document.querySelectorAll('.c-button');
 
